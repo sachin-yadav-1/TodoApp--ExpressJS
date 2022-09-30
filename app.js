222ccfea90cbc93340b6e48ac80e60d4840f131b
Sachin Yadav <sachin.y4dav@gmail.com>
2022-09-30 10:26:29 +0530

    ADD: Security packages

--- app.js
+++ app.js
@@ -5,4 +5,14 @@ const app = express();
 dotenv.config();
 app.use(express.json({ limit: '10kb' }));
 
+app.use(mongoSanitize());
+app.use(xss());
+app.use(helmet());
+const limiter = rateLimit({
+  max: 50,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests, please try again in an hour.',
+});
+app.use('/api', limiter);
+
 module.exports = app;
